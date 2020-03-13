accdceed9fc2f17d57b133a865834c383284d66e
harumaruge <hally1230po@gmail.com>
2020-03-13 12:44:47 +0000

    contact added

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -22,6 +22,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
   
 end
